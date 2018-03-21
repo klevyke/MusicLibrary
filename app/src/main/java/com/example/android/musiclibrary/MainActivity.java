e38a326d859cb615c08684efedc961bf6cdde03f
Kopacz Levente <office@imageservices.ro>
2018-03-21 11:37:30 +0200

    TrackList ArrayList populated

--- app/src/main/java/com/example/android/musiclibrary/MainActivity.java
+++ app/src/main/java/com/example/android/musiclibrary/MainActivity.java
@@ -64,5 +64,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ArrayList<Song> trackList = new ArrayList<Song>();
+        trackList.add(new Song("Michael Jackson", "Dirty Diana", "pop"));
+        trackList.add(new Song("Metallica", "Enter Sadman", "rock"));
+        trackList.add(new Song("Armin van Buuren feat Josh Cumbee", "Sunny Days", "trance"));
+        trackList.add(new Song("Scorpions", "Wind of Change", "rock"));
+        trackList.add(new Song("Avicii", "Levels", "club"));
+        trackList.add(new Song("Skrillex", "First Of The Year (Equinox)", "dubstep"));
+        trackList.add(new Song("Katie Melua", "The Closest Thing To Crazy", "blues"));
+        trackList.add(new Song("Pink Floyd", "Another brick in the wall", "rock"));
+        trackList.add(new Song("Tiesto", "Lethal Industry", "trance"));
+        trackList.add(new Song("R. Kelly", "Thoia Thoing", "R&B"));
+        trackList.add(new Song("Guns'n Roses", "Don't cry", "rock"));
+        trackList.add(new Song("Kygo", "Firestone", "deep house"));
+        trackList.add(new Song("Narcotic", "Liquido", "rock"));
+        trackList.add(new Song("Lost Frequencies", "Are you with me", "deep house"));
+        trackList.add(new Song("Cosmic Gate", "Exploration Of Space", "trance"));
+        trackList.add(new Song("Bon Jovi", "It's My Live", "rock"));
+
     }
 }
