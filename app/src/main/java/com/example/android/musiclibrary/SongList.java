9ab8cba9e8a0bfffaa883ba5aaa71532c4db14ee
Kopacz Levente <office@imageservices.ro>
2018-03-21 18:11:03 +0200

    SongList class created

---
+++ app/src/main/java/com/example/android/musiclibrary/SongList.java
@@ -0,0 +1,44 @@
+package com.example.android.musiclibrary;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Levy on 21.03.2018.
+ */
+
+public class SongList {
+    private ArrayList<Song> mSongs;
+    /**
+     * Constructor
+     * @return
+     */
+    public SongList () {
+        mSongs = populateSongList();
+    }
+
+    public ArrayList<Song> getSongs(){
+        return mSongs;
+    }
+
+    public ArrayList<Song> populateSongList() {
+        ArrayList<Song> trackList = new ArrayList<Song>();
+        trackList.add(new Song("Michael Jackson", "Dirty Diana", "pop"));
+        trackList.add(new Song("Metallica", "Enter Sadman", "rock"));
+        trackList.add(new Song("Armin van Buuren feat Josh Cumbee", "Sunny Days", "trance"));
+        trackList.add(new Song("Scorpions", "Wind of Change", "rock"));
+        trackList.add(new Song("Avicii", "Levels", "club"));
+        trackList.add(new Song("Skrillex", "First Of The Year (Equinox)", "dubstep"));
+        trackList.add(new Song("Katie Melua", "The Closest Thing To Crazy", "blues"));
+        trackList.add(new Song("Pink Floyd", "Another brick in the wall", "rock"));
+        trackList.add(new Song("Tiesto", "Lethal Industry", "trance"));
+        trackList.add(new Song("R. Kelly", "Thoia Thoing", "R&B"));
+        trackList.add(new Song("Guns'n Roses", "Don't cry", "rock"));
+        trackList.add(new Song("Kygo", "Firestone", "deep house"));
+        trackList.add(new Song("Narcotic", "Liquido", "rock"));
+        trackList.add(new Song("Lost Frequencies", "Are you with me", "deep house"));
+        trackList.add(new Song("Cosmic Gate", "Exploration Of Space", "trance"));
+        trackList.add(new Song("Bon Jovi", "It's My Live", "rock"));
+
+        return trackList;
+    }
+}
